f920e2332a1ed6f9a95997bad87f6b4dc047c716
wouter <31807225+wouter173@users.noreply.github.com>
2021-05-31 16:11:39 +0200

    Fixed Header/index fixed the weird snappy movement on the background

--- src/components/Pages/Home/Header/index.tsx
+++ src/components/Pages/Home/Header/index.tsx
@@ -11,16 +11,14 @@ import ice from './planets/ice.png';
 import MediaContext from '../../../Shared/Context/MediaContext';
 
 export default function Header() {
-
-	const TOTAL = 50;
 	const [pos, setPos] = useState<{x: number, y: number}>({x:0,y:0});
 	const media = useContext(MediaContext);
 
 	useEffect(() => {
 		document.addEventListener('mousemove', mouse => {
 			setPos({
-				x: (TOTAL - (mouse.clientX - window.innerWidth/2) * 0.01), 
-				y: (TOTAL - (mouse.clientY - window.innerHeight/2) * 0.01)
+				x: (mouse.clientX - window.innerWidth/2) * 0.01,
+				y: (mouse.clientY - window.innerHeight/2) * 0.01,
 			});
 		});
 	}, []);
@@ -28,7 +26,7 @@ export default function Header() {
 	return (
 		<header>
 			<div className="background">
-				<img src={dark} alt="" style={media.isDesktop? {left: 40 - pos.x/3 +'px', top: 40 - pos.y/3 +'px'}: {left: '10px', top: '30px', width: '140px'}}/>
+				<img src={dark} alt="" style={media.isDesktop? {left: 40 - pos.x/3 + 'px', top: 40 - pos.y/3 +'px'}: {left: '10px', top: '30px', width: '140px'}}/>
 				<img src={ice} alt="" style={media.isDesktop?{right: 200 + pos.x/6 + 'px', top: 100 - pos.y/6 + 'px', width: '300px'}: {right: '0px', top: '70px', width: '200px'}}/> {/*: {right: '-210px', bottom: '-100px', width: '800px'}}/>*/}
 				<img src={earthish} alt="" style={ media.isDesktop? {right: -80 + pos.x + 'px', bottom: -20 + pos.y + 'px', width: '1000px'}: {right: '-210px', bottom: '-100px', width: '800px'}}/>
 			</div>
