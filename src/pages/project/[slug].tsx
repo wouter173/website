c161d519d3c215f75274ad5a49bb3ce563c2a4fa
wouter <wouter@debruijn.dev>
2022-07-07 17:17:42 +0200

    feat: seo optimized project pages

--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -9,6 +9,7 @@ import Content from 'components/Article/Content';
 import MetaData from 'components/Article/MetaData';
 import Nav from 'components/Article/Nav';
 import { getApiUri } from 'misc/utils';
+import Head from 'next/head';
 
 export default function Project() {
 	const router = useRouter();
@@ -23,18 +24,32 @@ export default function Project() {
 	const scrollRef = useRef(null);
 	const metadataTitleRef = useRef(null);
 
+	const title = 'Wouter de Bruijn | ' + project.title;
+
 	return (
-		<div id="Article" className="max-h-screen overflow-auto bg-bermuda" ref={scrollRef}>
-			<div className="mx-auto mt-8 grid w-11/12 grid-flow-row gap-10 sm:mx-auto sm:w-1/2 sm:px-0">
-				<Nav refs={{ scrollRef, metadataTitleRef }} slug={id} />
-				<div
-					className="card h-96 bg-bermuda-card bg-cover bg-center"
-					style={{ backgroundImage: `url(${getApiUri()}${project.thumbnail.url ?? ''})` }}
-				/>
-				<MetaData metadata={project} ref={metadataTitleRef} />
-				<Content className="card" document={project.content.document ?? []} />
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="theme-color" content={project.themeColor} />
+				<meta name="description" content={project.description} />
+
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={title} />
+				<meta property="og:description" content={project.description} />
+				<meta property="og:image" content={getApiUri() + project.thumbnail.url} />
+				<meta property="og:url" content={'https://wouterdb.nl' + router.asPath} />
+
+				<title>{title}</title>
+			</Head>
+			<div id="Article" className="max-h-screen overflow-auto bg-bermuda" ref={scrollRef}>
+				<div className="mx-auto mt-8 grid w-11/12 grid-flow-row gap-10 sm:mx-auto sm:w-1/2 sm:px-0">
+					<Nav refs={{ scrollRef, metadataTitleRef }} slug={id} />
+					<div className="card h-96 bg-bermuda-card bg-cover bg-center" style={{ backgroundImage: `url(${getApiUri() + project.thumbnail.url})` }} />
+					<MetaData metadata={project} ref={metadataTitleRef} />
+					<Content className="card" document={project.content.document ?? []} />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
