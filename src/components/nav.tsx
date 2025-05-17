fe0142fae3a8b51ae7ec791ddee56c4ccb1e00bc
wouter <wouter@debruijn.dev>
2025-05-17 15:14:07 +0200

    feat: nav improvements

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -10,37 +10,37 @@ export function Nav() {
 
   return (
     <motion.nav layoutRoot className="relative z-20 mx-auto max-w-4xl px-6 pt-6 lg:px-0 lg:pt-16">
-      <div className="flex w-min items-center gap-6 rounded-full contain-layout">
-        <Link href="/" className="relative">
+      <div className="flex w-min items-center rounded-full contain-layout">
+        <Link href="/" className="relative pr-3">
           <Logo className="relative" />
         </Link>
-        <Link href="/" className="relative">
+        <Link href="/" className="group relative px-3">
           {pathname === '/' && (
             <motion.div
               layoutId="active-tab"
-              className="absolute -inset-x-2 -inset-y-0.25 -z-10 rounded-full border border-neutral-200 bg-neutral-100"
+              className="absolute inset-x-0 top-1/2 -z-10 h-7 -translate-y-1/2 rounded-full border border-neutral-200 bg-neutral-100"
             />
           )}
-          <span className="relative z-20 text-sm font-medium">Home</span>
+          <span className="relative z-20 text-sm font-medium transition-all group-hover:opacity-70">Home</span>
         </Link>
 
-        <Link href={'/work'} className="relative">
+        <Link href={'/work'} className="group relative px-3">
           {pathname === '/work' && (
             <motion.div
               layoutId="active-tab"
-              className="absolute -inset-x-2 -inset-y-0.25 -z-10 rounded-full border border-neutral-200 bg-neutral-100"
+              className="absolute inset-x-0 top-1/2 -z-10 h-7 -translate-y-1/2 rounded-full border border-neutral-200 bg-neutral-100"
             />
           )}
-          <span className="relative z-20 text-sm font-medium">Work</span>
+          <span className="relative z-20 text-sm font-medium transition-all group-hover:opacity-70">Work</span>
         </Link>
-        <Link href={'/posts'} className="relative">
+        <Link href={'/posts'} className="group relative px-3">
           {pathname.startsWith('/posts') && (
             <motion.div
               layoutId="active-tab"
-              className="absolute -inset-x-2 -inset-y-0.25 -z-10 rounded-full border border-neutral-200 bg-neutral-100"
+              className="absolute inset-x-0 top-1/2 -z-10 h-7 -translate-y-1/2 rounded-full border border-neutral-200 bg-neutral-100"
             />
           )}
-          <span className="relative z-20 text-sm font-medium">Posts</span>
+          <span className="relative z-20 text-sm font-medium transition-all group-hover:opacity-70">Posts</span>
         </Link>
       </div>
     </motion.nav>
