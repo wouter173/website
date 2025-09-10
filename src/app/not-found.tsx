dc1003c96c0af84b961b4bb1c81e0bc69606475d
wouter <wouter@debruijn.dev>
2025-09-10 15:55:25 +0200

    fix: update not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,14 +5,12 @@ import Link from 'next/link'
 export default async function NotFound() {
   return (
     <>
-      <main className="relative z-10 flex h-[90vh] flex-col items-center justify-center">
-        <div className="rounded-lg border border-neutral-200 bg-white px-6 py-6">
-          <h1 className="text-xl font-bold text-black">404 - Not Found</h1>
-          <p className="mt-2 text-sm">The page you are looking for does not exist.</p>
-          <Button variant={'primary'} className="mt-8" asChild>
-            <Link href="/posts">Check out my posts</Link>
-          </Button>
-        </div>
+      <main className="relative z-10 flex h-[90vh] flex-col items-center justify-center pb-10">
+        <h1 className="text-xl font-bold text-white">404 - Not Found</h1>
+        <p className="mt-2 text-sm">The page you are looking for does not exist.</p>
+        <Button variant={'primary'} className="mt-8" asChild>
+          <Link href="/posts">Check out my posts</Link>
+        </Button>
       </main>
       <Footer />
     </>
